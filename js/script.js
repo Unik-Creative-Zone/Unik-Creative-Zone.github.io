68de0614a1952240f0354791fe6c5a9f041036cb
UnikCreative <dejoesco@gmail.com>
2021-06-21 15:04:20 +0100

    modified site

--- js/script.js
+++ js/script.js
@@ -1,49 +1,3 @@
-// window.addEventListener("DOMContentLoaded"), function () {
-
-//   var form = document.getElementById("my-form");
-//   var status = document.getElementById("status");
-
-//   // Success and Error function for after the form is submisted
-
-//   function success() {
-//     form.reset();
-//     status.classList.add('success');
-//     status.innerHTML = "Thanks!";
-//   }
-
-//   function error() {
-//     status.classList.add('error');
-//     status.innerHTML = "Oops! Theres was a problem";
-//   }
-
-//   // handle the form submission event
-
-//   form.addEventListener("submit", function (ev) {
-//     ev.preventDefault();
-//     var data = new FormData(form);
-//     ajax(form.method, form.action, data, success, error);
-//   });
-// };
-
-
-//   function ajax(method, url, data, success, error) {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open(method, url);
-//     xhr.setRequestHeader("Accept", "application/json");
-//     xhr.onreadystatechange = function () {
-//       if (xhr.readyState !== XMLHttpRequest.DONE) return;
-//       if (xhr.status === 200) {
-//         success(xhr.response, xhr.responseType);
-//       } else {
-//         error(xhr.status, xhr.response, xhr.responseType);
-//       }
-//     };
-//     xhr.send(data);
-//   }
-
-
-
-
 
 var form = document.getElementById("my-form");
     
@@ -59,12 +13,12 @@ var form = document.getElementById("my-form");
         }
 
       }).then(response => {
-        status.innerHTML = "Thanks for your submission!";
+         status.innerHTML = alert ("Thanks for your submission!");
         form.reset()
         status.classList.add('success');
       }).catch(error => {
         status.classList.add('error');
-        status.innerHTML = "Oops! There was a problem submitting your form"
+        status.innerHTML = alert("Oops! There was a problem submitting your form");
       });
     }
     form.addEventListener("submit", handleSubmit)
